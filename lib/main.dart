9c798049ffcfab18499cac67a95c5c87ecb1c69f
Rafael <rafaelvicio@icloud.com>
2019-10-19 11:44:38 -0300

    install location

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 import 'pages/NewGame.dart';
 import 'pages/JoinGame.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  Position position = await Geolocator()
+      .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+  print(position);
+  return runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
