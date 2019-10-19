5668d637a7c745ffea7e6a11d16e74310200470d
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2019-10-19 01:41:35 -0300

    add initial pages

---
+++ lib/pages/JoinGame.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class JoinGame extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Join Game'),
+      ),
+      body: Text('Hello Join Game'),
+    );
+  }
+}
