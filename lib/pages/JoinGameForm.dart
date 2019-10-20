33f20d6fe83f2167bd87fdefd15f49d1db02f03f
Vitor Ribeiro <vitor.ribeiro@figure-eight.com>
2019-10-19 23:51:24 -0300

    adding fonts

--- lib/pages/JoinGameForm.dart
+++ lib/pages/JoinGameForm.dart
@@ -7,7 +7,9 @@ class JoingGameForm extends StatefulWidget {
 
 class _JoinGameState extends State<JoingGameForm> {
   final _formKey = GlobalKey<FormState>();
+  final _codeKey = GlobalKey<FormState>();
   String nickname = "";
+  String code = "";
   @override
   void initState() {
     super.initState();
@@ -38,7 +40,7 @@ class _JoinGameState extends State<JoingGameForm> {
             child: Text(
               "NICKNAME",
               style: TextStyle(
-                  fontFamily: 'Montreal-Bold', 
+                  fontFamily: 'Montreal', 
                   fontSize: 12, 
                   color: Color.fromRGBO(114, 114, 114, 0.7),
                   fontWeight: FontWeight.bold
@@ -70,6 +72,65 @@ class _JoinGameState extends State<JoingGameForm> {
               width: 260,
               height: 50,
               margin: const EdgeInsets.only(top: 10),
+          ),
+          new Container(
+            child: Text(
+              "CÓDIGO DE ACESSO",
+              style: TextStyle(
+                  fontFamily: 'Montreal', 
+                  fontSize: 12, 
+                  color: Color.fromRGBO(114, 114, 114, 0.7),
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            margin: const EdgeInsets.only(top: 25),
+          ),
+          new Container(
+            child:
+              Form(
+                key: _codeKey,
+                child: TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Digite o código do jogo';
+                    }
+                    return null;
+                  },
+                  onChanged: (text) {
+                    code = text;
+                  },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(const Radius.circular(25.0)),
+                      ),
+                    )
+                ),
+              ),
+              width: 260,
+              height: 50,
+              margin: const EdgeInsets.only(top: 10),
+            ),
+          new Container(
+            child: new RaisedButton(
+              onPressed: () {
+                print("clicado");
+              },
+              shape: RoundedRectangleBorder(
+                borderRadius: new BorderRadius.circular(25.0),
+                ), color: Color.fromRGBO(254, 133, 110, 1),
+              child: Text(
+                'Entrar',
+                  style: TextStyle(
+                    fontFamily: 'Montreal', 
+                    fontSize: 16, 
+                    color: Color.fromRGBO(255, 255, 255, 1),
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+            ),
+            width: 260,
+            height: 50,
+            margin: const EdgeInsets.only(top: 60),
           )
         ],),
       ),
