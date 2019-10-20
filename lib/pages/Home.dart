69f1a2b1ff6367e14609b6dddc8e98d2f242ba7e
Rafael <rafaelvicio@icloud.com>
2019-10-19 23:22:36 -0300

    bg

--- lib/pages/Home.dart
+++ lib/pages/Home.dart
@@ -8,45 +8,51 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: <Widget>[
-            Padding(padding: const EdgeInsets.all(80.0)),
-            ImageRotate(),
-            Image.asset('assets/images/orbitar.png'),
-            Padding(padding: const EdgeInsets.all(90.0)),
-            Expanded(
-              child: Column(
-                children: <Widget>[
-                  MaterialButton(
-                    child: Text('Entrar em um jogo',
-                        style: TextStyle(fontSize: 16.0)),
-                    onPressed: doSomething,
-                    color: Color.fromRGBO(254, 133, 110, 1),
-                    textColor: Colors.white,
-                    height: 50.0,
-                    minWidth: 300.0,
-                    shape: StadiumBorder(),
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: MaterialButton(
-                        child:
-                            Text('Novo jogo', style: TextStyle(fontSize: 16.0)),
-                        onPressed: doSomething,
-                        textColor: Color.fromRGBO(254, 133, 110, 1),
-                        height: 50.0,
-                        minWidth: 300.0,
-                      ))
-                ],
-              ),
+        body: DecoratedBox(
+            position: DecorationPosition.background,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/bg.png'), fit: BoxFit.cover),
             ),
-          ],
-        ),
-      ),
-    ));
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    Padding(padding: const EdgeInsets.all(80.0)),
+                    ImageRotate(),
+                    Image.asset('assets/images/orbitar.png'),
+                    Padding(padding: const EdgeInsets.all(90.0)),
+                    Expanded(
+                      child: Column(
+                        children: <Widget>[
+                          MaterialButton(
+                            child: Text('Entrar em um jogo',
+                                style: TextStyle(fontSize: 16.0)),
+                            onPressed: doSomething,
+                            color: Color.fromRGBO(254, 133, 110, 1),
+                            textColor: Colors.white,
+                            height: 50.0,
+                            minWidth: 300.0,
+                            shape: StadiumBorder(),
+                          ),
+                          Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: MaterialButton(
+                                child: Text('Novo jogo',
+                                    style: TextStyle(fontSize: 16.0)),
+                                onPressed: doSomething,
+                                textColor: Color.fromRGBO(254, 133, 110, 1),
+                                height: 50.0,
+                                minWidth: 300.0,
+                              ))
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )));
   }
 }
 
