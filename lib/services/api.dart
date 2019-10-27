8e04ac52f268a588fbcad8c721612a89282a07e5
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2019-10-27 12:07:37 -0300

    Create api service

---
+++ lib/services/api.dart
@@ -0,0 +1,12 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../models/satellite.dart';
+
+final dataBaseUrl = 'http://10.0.2.2:8000/api/v1';
+
+Future<List<Satellite>> getSatellites() async {
+  final response = await http.get('$dataBaseUrl/satellites');
+  final decodedBody = json.decode(response.body);
+  return List<Satellite>.from(decodedBody.map((s) => Satellite.fromJson(s)));
+}
