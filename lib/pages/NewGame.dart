2dbf14f3b4a7bc0b20cae15103555a83f0ed2bde
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2019-10-27 14:13:34 -0300

    add NewGame design

--- lib/pages/NewGame.dart
+++ lib/pages/NewGame.dart
@@ -33,18 +33,43 @@ class _NewGameState extends State<NewGame> {
               return Text('Error on fetching data');
             }
             return SafeArea(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Card(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Text('sdf'),
+              child: Container(
+                color: Colors.white,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Card(
+                      margin: EdgeInsets.all(0),
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(30, 19, 26, 19),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: IconButton(
+                                icon: Image.asset(
+                                  'assets/images/close.png',
+                                ),
+                                onPressed: () => Navigator.pop(context),
+                              ),
+                            ),
+                            Text(
+                              'Monte o seu jogo',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Color.fromRGBO(90, 94, 96, 1),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                  Expanded(child: _satellitesListWidget(snapshot.data)),
-                ],
+                    Expanded(child: _satellitesListWidget(snapshot.data)),
+                  ],
+                ),
               ),
             );
           default:
@@ -56,14 +81,36 @@ class _NewGameState extends State<NewGame> {
 }
 
 _satellitesListWidget(List<Satellite> satellites) {
-  return ListView.builder(
+  return ListView.separated(
+    padding: EdgeInsets.all(0),
     itemCount: satellites.length,
+    separatorBuilder: (context, index) => Divider(),
     itemBuilder: (context, position) {
       return GestureDetector(
         child: Card(
+          color: Colors.white,
           child: Padding(
             padding: EdgeInsets.all(16),
-            child: Container(child: Text(satellites[position].name)),
+            child: Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 22, 0),
+                  child: Container(
+                    width: 26,
+                    height: 26,
+                    decoration: ShapeDecoration(
+                      shape: CircleBorder(
+                        side: BorderSide(
+                          width: 1,
+                          color: Color.fromRGBO(144, 144, 144, 1),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Text(satellites[position].name),
+              ],
+            ),
           ),
         ),
         onTap: () => print('tapped'),
