56b3a0defe3210f0e5184e40078bcac83b8cd599
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2019-10-27 16:14:23 -0300

    Implement Satellite selection

--- lib/pages/NewGame.dart
+++ lib/pages/NewGame.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:async/async.dart';
 
 import 'package:museum/models/satellite.dart';
 import 'package:museum/services/api.dart' as api;
@@ -10,77 +11,80 @@ class NewGame extends StatefulWidget {
 }
 
 class _NewGameState extends State<NewGame> {
-  List<Satellite> satellites;
-  List<int> selections;
+  List<Satellite> satellites = [];
+  List<bool> selections = [];
 
-  @override
-  void initState() {
-    super.initState();
+  addOrRemoveSatellite(int position) {
+    selections[position] = !selections[position];
     setState(() {
-      satellites = [];
-      selections = [];
+      selections = selections;
     });
   }
 
   @override
+  void initState() {
+    api.getSatellites().then((data) => {
+          setState(() {
+            satellites = data;
+            selections = List.filled(data.length, false);
+          })
+        });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Satellite>>(
-      future: api.getSatellites(),
-      builder: (BuildContext context, AsyncSnapshot<List<Satellite>> snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.done:
-            if (snapshot.hasError) {
-              return Text('Error on fetching data');
-            }
-            return SafeArea(
-              child: Container(
-                color: Colors.white,
+    return SafeArea(
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Card(
+              margin: EdgeInsets.all(0),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(30, 19, 26, 19),
                 child: Column(
-                  mainAxisSize: MainAxisSize.min,
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: <Widget>[
-                    Card(
-                      margin: EdgeInsets.all(0),
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(30, 19, 26, 19),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: <Widget>[
-                            Align(
-                              alignment: Alignment.centerRight,
-                              child: IconButton(
-                                icon: Image.asset(
-                                  'assets/images/close.png',
-                                ),
-                                onPressed: () => Navigator.pop(context),
-                              ),
-                            ),
-                            Text(
-                              'Monte o seu jogo',
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Color.fromRGBO(90, 94, 96, 1),
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: IconButton(
+                        icon: Image.asset(
+                          'assets/images/close.png',
                         ),
+                        onPressed: () => Navigator.pop(context),
                       ),
                     ),
-                    Expanded(child: _satellitesListWidget(snapshot.data)),
+                    Text(
+                      'Monte o seu jogo',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Color.fromRGBO(90, 94, 96, 1),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ],
                 ),
               ),
-            );
-          default:
-            return Text('Data Fetched');
-        }
-      },
+            ),
+            Expanded(
+              child: _satellitesListWidget(
+                satellites,
+                selections,
+                addOrRemoveSatellite,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
-_satellitesListWidget(List<Satellite> satellites) {
+_satellitesListWidget(List<Satellite> satellites, List<bool> selections,
+    Function onSatteliteSelected) {
   return ListView.separated(
     padding: EdgeInsets.all(0),
     itemCount: satellites.length,
@@ -96,16 +100,21 @@ _satellitesListWidget(List<Satellite> satellites) {
                 Padding(
                   padding: const EdgeInsets.fromLTRB(0, 0, 22, 0),
                   child: Container(
+                    child: selections[position]
+                        ? Image.asset('assets/images/check.png')
+                        : null,
                     width: 26,
                     height: 26,
-                    decoration: ShapeDecoration(
-                      shape: CircleBorder(
-                        side: BorderSide(
-                          width: 1,
-                          color: Color.fromRGBO(144, 144, 144, 1),
-                        ),
-                      ),
-                    ),
+                    decoration: selections[position]
+                        ? null
+                        : ShapeDecoration(
+                            shape: CircleBorder(
+                              side: BorderSide(
+                                width: 1,
+                                color: Color.fromRGBO(144, 144, 144, 1),
+                              ),
+                            ),
+                          ),
                   ),
                 ),
                 Text(satellites[position].name),
@@ -113,7 +122,7 @@ _satellitesListWidget(List<Satellite> satellites) {
             ),
           ),
         ),
-        onTap: () => print('tapped'),
+        onTap: () => onSatteliteSelected(position),
       );
     },
   );
