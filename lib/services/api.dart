fa4b58e2b747fe5f0556ed71350f3dbc75180001
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2019-10-27 18:51:58 -0300

    Add createGame api method

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -4,9 +4,19 @@ import 'dart:convert';
 import '../models/satellite.dart';
 
 final dataBaseUrl = 'http://10.0.2.2:8000/api/v1';
+final backendBaseUrl = 'http://10.0.2.2:3000';
 
 Future<List<Satellite>> getSatellites() async {
   final response = await http.get('$dataBaseUrl/satellites');
   final decodedBody = json.decode(utf8.decode(response.bodyBytes));
   return List<Satellite>.from(decodedBody.map((s) => Satellite.fromJson(s)));
+}
+
+Future<String> createGame(List<int> satellitesIds) async {
+  final response = await http.post(
+    '$backendBaseUrl/games',
+    body: {'satellites': json.encode(satellitesIds)},
+  );
+  final parsedBody = json.decode(utf8.decode(response.bodyBytes));
+  return parsedBody['code'];
 }
