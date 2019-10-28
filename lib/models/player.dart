5490bc35ed693b797f3cb9fdac1bd6a1f0062fc2
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2019-10-28 00:03:41 -0300

    fix Player parser

--- lib/models/player.dart
+++ lib/models/player.dart
@@ -2,14 +2,21 @@ class Player {
   int id;
   int gameId;
   String nickname;
-  List<int> collectedSatellites;
+  List<String> availableSatellites;
+  List<String> collectedSatellites;
 
-  Player({this.id, this.gameId, this.nickname, this.collectedSatellites});
+  Player(
+      {this.id,
+      this.gameId,
+      this.nickname,
+      this.collectedSatellites,
+      this.availableSatellites});
 
   factory Player.fromJson(Map<String, dynamic> json) => Player(
         id: json['id'],
         gameId: json['game_id'],
         nickname: json['nickname'],
-        collectedSatellites: List<int>.from(json['collected_satellites']),
+        collectedSatellites: List<String>.from(json['collected_satellites']),
+        availableSatellites: List<String>.from(json['game']['satellites']),
       );
 }
